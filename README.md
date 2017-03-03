25833dd17d3a3cc8c9b52088eb732ec91f406ee8
Ryan McGuire <ryan@enigmacurry.com>
2017-03-03 00:48:49 -0500

    More api implemented.

--- README.md
+++ README.md
@@ -20,12 +20,57 @@ Run Demo
 Features
 ----------
 
-Implemented:
- - [x] record subscriptions
- - [x] record updates
- 
-Todo:
- - [ ] snapshots
- - [ ] events
- - [ ] lists
- - [ ] deletes
+Deepstream API Implementation:
+  - Records:
+    - [x] record.subscribe
+       - once subscribed, will emit events:
+         - [x] record.discard
+         - [x] record.delete
+         - [x] record.error
+    - [x] record.set
+    - [x] record.get
+    - [x] record.snapshot
+    - [x] record.discard
+    - [x] record.delete
+    - [x] record.listen
+    - [ ] record.unsubscribe 
+      - Not implemented. Would need to specify an id to subscribe() and 
+        then pass the same id on unsubscribe, in order to track the callback.
+    
+  - Lists:
+    - [x] list.subscribe
+      - once subscribed, will emit events:
+         - [x] list.discard
+         - [x] list.delete
+         - [x] list.error
+         - [x] list.entry-added
+         - [x] list.entry-moved
+         - [x] list.entry-removed
+    - [x] list.getEntries
+    - [x] list.setEntries
+    - [x] list.addEntry
+    - [x] list.removeEntry
+    - [x] list.discard
+    - [x] list.delete
+    - [x] list.unsubscribe
+    
+    
+  - Events:
+    - [ ] event.subscribe
+    - [ ] event.unsubscribe
+    - [ ] event.emit
+    - [ ] event.listen
+    
+  - Presence:
+    - [ ] presence.subscribe
+    - [ ] presence.unsubscribe
+    - [ ] presence.getAll
+    
+  - RPC:
+    - [ ] rpc.make
+    - [ ] rpc.provide - I think this makes no sense to implement in cycle?
+
+TODO
+-----
+
+ [ ] - Testing... lol.
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ export function makeDeepstreamDriver(url, auth={}, options={}) {
   function deepstreamDriver(action$, streamAdapter) {
     const cachedRecords = {}
     const cachedLists = {}
-
+    
     // Internal event emitter to delegate between action 
     const events = new EventEmitter()
     const emit = (data) => events.emit('deepstream-event', data)
@@ -37,30 +37,218 @@ export function makeDeepstreamDriver(url, auth={}, options={}) {
       })
     }
 
-    const recordSubscription$ = action$.filter(intent => intent.action === 'record.subscribe')
+    const recordSubscription$ = action$.filter(intent => intent.action === 'record.subscribe'
+                                               && intent.name != undefined)
     const recordSubscriptionListener = recordSubscription$.addListener({
-      next: action => {
-        getRecord(action.name).then(record => {
+      next: intent => {
+        getRecord(intent.name).then(record => {
           record.subscribe(data => {
-            emit({event:'record.change', name:action.name, data:data})
+            emit({event:'record.change', name:record.name, data:data})
           }, true)
+          record.on('discard', () => {
+            emit({event:'record.discard', name:record.name})
+          })
+          record.on('delete', () => {
+            emit({event:'record.delete', name:record.name})
+          })
+          record.on('error', (err) => {
+            emit({event:'record.error', name:record.name, error:err})
+          })          
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+    
+    const recordGet$ = action$.filter(intent => intent.action === 'record.get'
+                                      && intent.name != undefined)
+    const recordGetListener = recordGet$.addListener({
+      next: intent => {
+        getRecord(intent.name).then(record => {
+          emit({event:'record.get', name: record.name, data: record})
         })
       },
       error: () => {},
       complete: () => {}      
     })
 
-    const recordSet$ = action$.filter(intent => intent.action === 'record.set')
+    const recordSnapshot$ = action$.filter(intent => intent.action === 'record.snapshot'
+                                           && intent.name != undefined)
+    const recordSnapshotListener = recordSnapshot$.addListener({
+      next: intent => {
+        client.record.snapshot(intent.name, record => {
+          emit({event:'record.snapshot',  name: record.name, data: record})
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    
+    const recordSet$ = action$.filter(intent => intent.action === 'record.set'
+                                      && intent.name != undefined
+                                      && intent.data != undefined)
     const recordSetListener = recordSet$.addListener({
-      next: action => {
-        getRecord(action.name).then(record => {
-          record.set(action.path, action.data)
+      next: intent => {
+        getRecord(intent.name).then(record => {
+          record.set(intent.path, intent.data)
         })
       },
       error: () => {},
       complete: () => {}      
     })
 
+    const recordDelete$ = action$.filter(intent => intent.action === 'record.delete'
+                                         && intent.name != undefined)
+    const recordDeleteListener = recordDelete$.addListener({
+      next: intent => {
+        getRecord(intent.name).then(record => {
+          record.unsubscribe()
+          record.delete()
+          delete cachedRecords[record.name]
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    const recordDiscard$ = action$.filter(intent => intent.action === 'record.discard'
+                                          && intent.name != undefined)
+    const recordDiscardListener = recordDiscard$.addListener({
+      next: intent => {
+        getRecord(intent.name).then(record => {
+          record.unsubscribe()
+          record.discard()
+          delete cachedRecords[record.name]
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    const recordListen$ = action$.filter(intent => intent.action === 'record.listen'
+                                         && intent.pattern != undefined)
+    const recordListenListener = recordListen$.addListener({
+      next: intent => {
+        client.record.listen(intent.pattern, (match, isSubscribed, response) => {
+          response.accept()
+          emit({event:'record.listen', match, isSubscribed})
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+    
+    const listSubscription$ = action$.filter(intent => intent.action === 'list.subscribe'
+                                             && intent.name != undefined)
+    const listSubscriptionListener = listSubscription$.addListener({
+      next: intent => {
+        getList(intent.name).then(list => {
+          list.subscribe(data => {
+            emit({event:'list.change', name:list.name, data:data})
+          }, true)
+          list.on('discard', () => {
+            emit({event:'list.discard', name:list.name})
+          })
+          list.on('delete', () => {
+            emit({event:'list.delete', name:list.name})
+          })
+          list.on('error', (err) => {
+            emit({event:'list.error', name:list.name, error:err})
+          })
+          list.on('entry-added', (entry, position) => {
+            emit({event:'list.entry-added', name:list.name, entry, position})
+          })
+          list.on('entry-moved', (entry, position) => {
+            emit({event:'list.entry-moved', name:list.name, entry, position})
+          })
+          list.on('entry-removed', (entry, position) => {
+            emit({event:'list.entry-removed', name:list.name, entry, position})
+          })
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    const listGetEntries$ = action$.filter(intent => intent.action === 'list.getEntries'
+                                           && intent.name != undefined)
+    const listGetEntriesListener = listGetEntries$.addListener({
+      next: intent => {
+        getList(intent.name).then(list => {
+          emit({event:'list.getEntries', name: list.name, data: list.getEntries()})
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    const listSetEntries$ = action$.filter(intent => intent.action === 'list.setEntries'
+                                           && intent.entries != undefined
+                                           && intent.name != undefined)
+    const listSetEntriesListener = listSetEntries$.addListener({
+      next: intent => {
+        getList(intent.name).then(list => {
+          list.setEntries(intent.entries)
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    const listAddEntry$ = action$.filter(intent => intent.action === 'list.addEntry'
+                                         && intent.name != undefined
+                                         && intent.entry != undefined)
+    const listAddEntryListener = listAddEntry$.addListener({
+      next: intent => {
+        getList(intent.name).then(list => {
+          list.addEntry(intent.entry, intent.index)
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    const listRemoveEntry$ = action$.filter(intent => intent.action === 'list.removeEntry'
+                                            && intent.name != undefined
+                                            && intent.entry != undefined)
+    const listRemoveEntryListener = listRemoveEntry$.addListener({
+      next: intent => {
+        getList(intent.name).then(list => {
+          list.removeEntry(intent.entry, intent.index)
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    const listDelete$ = action$.filter(intent => intent.action === 'list.delete'
+                                       && intent.name != undefined)
+    const listDeleteListener = listDelete$.addListener({
+      next: intent => {
+        getList(intent.name).then(list => {
+          list.unsubscribe()
+          list.delete()
+          delete cachedLists[list.name]
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
+
+    const listDiscard$ = action$.filter(intent => intent.action === 'list.discard'
+                                        && intent.name != undefined)
+    const listDiscardListener = listDiscard$.addListener({
+      next: intent => {
+        getList(intent.name).then(list => {
+          list.unsubscribe()
+          list.discard()
+          delete cachedLists[list.name]
+        })
+      },
+      error: () => {},
+      complete: () => {}      
+    })
     
     const effect$ = xs.create({
       start: listener => {
