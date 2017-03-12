600f2ce87f0101b96b8131df81c1c6dd63a59985
Ryan McGuire <ryan@enigmacurry.com>
2017-03-12 13:26:55 -0400

    render markdown in posts

--- examples/advanced/src/containers/Post.ts
+++ examples/advanced/src/containers/Post.ts
@@ -1,21 +1,37 @@
 import xs from 'xstream'
 import * as deepstream from '../actions/deepstream'
-import { h } from '@cycle/dom'
+import { VNode, h } from '@cycle/dom'
 import { Sources, Sinks } from '../types'
+import * as Markdown from 'markdown-it'
+import { toVNode } from 'snabbdom/tovnode'
+
+const markdown = new Markdown()
 
 function view(post) {
-  return h('div', JSON.stringify(post))
+  //Render post content from markdown to HTML.
+  const content = markdown.render(post.content)
+  //Manually create a DOM element and corece it into a snabbdom VNode:
+  const elm = document.createElement('div')
+  elm.innerHTML = content
+  return toVNode(elm)
 }
 
 export function Post(sources: Sources): Sinks {
   const { DOM, deep$, props$ } = sources
 
+  const deletedPost = {
+    content: '[deleted]'
+  }
+
+  // Output the post when it come back from deepstream -
+  // If the post is ever deleted, hide the post:
   const vdom$ = xs.combine(props$, deep$)
     .filter(([props, effect]) => effect.name === props.id)
-    .filter(([props, effect]) => effect.event === 'record.change' ||
+    .map(([props, effect]) => effect)
+    .filter(effect => effect.event === 'record.change' ||
       effect.event === 'record.delete')
-    .map(([props, effect]) => view(
-      effect.event === 'record.delete' ? '[deleted]' : effect.data))
+    .map(effect => view(
+      effect.event === 'record.delete' ? deletedPost : effect.data))
     .startWith(h('div', 'loading...'))
 
   // Request the post specified in props.id
