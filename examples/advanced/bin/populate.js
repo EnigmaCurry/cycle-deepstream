90575d19612277c15f5f5a8ce3da0b4dcce080ab
Ryan McGuire <ryan@enigmacurry.com>
2017-03-22 16:56:22 -0400

    [example] Autopopulate database on startup

--- examples/advanced/bin/populate.js
+++ examples/advanced/bin/populate.js
@@ -1,12 +1,59 @@
 const deepstream = require('deepstream.io-client-js')
+const fetch = require('node-fetch')
 
-const ds = deepstream('localhost:6020').login({ username: 'john', password: 'john' })
+const host = 'localhost:6020'
 
-const main = ds.record.getRecord('p/main')
-main.set({content:'# Hello Markdown\n\n * one\n * two\n * three\n'})
+function waitForDeepstream(host, retries = 30, wait=3000) {
+  return new Promise((accept, reject) => {
+    console.log(`Waiting for deepstream health-check (${retries} retries left) ...`)
+    fetch(`http://${host}/health-check`)
+      .catch(err => {
+        if (retries > 0) {
+          setTimeout(() => {
+            waitForDeepstream(host, retries - 1)
+              .then(res => accept())
+              .catch(err => reject("Deepstream never responded"))
+          }, wait)
+        } else {
+          reject("Deepstream never responded")
+        }
+      })
+      .then(res => {
+        if (res !== undefined) {
+          console.log("Deepstream ready.")
+          accept()
+        }
+      })
+    
+  })
+}
 
-const child1 = ds.record.getRecord('p/main/child1')
-child1.set({content:'This is child1'})
+waitForDeepstream(host)
+  .then(res => {
+    const ds = deepstream(host).login({ username: 'john', password: 'john' })
+    ds.on('connectionStateChanged', state => {
+      if (state === deepstream.CONSTANTS.CONNECTION_STATE.OPEN) {
+        // Populate the database with initial test data
+        ds.record.getRecord('p/main').whenReady(main => {
+          if (main.get().content === undefined) {
+            main.set({content:'# Hello Markdown\n\n * one\n * two\n * three\n'})
+            const child1 = ds.record.getRecord('p/main/child1')
+            child1.set({content:'This is child1'})
 
-const children = ds.record.getList('p/main/children')
-children.addEntry(child1.name)
+            const children = ds.record.getList('p/main/children')
+            children.addEntry(child1.name)
+            console.log("Populated database")
+          } else {
+            console.log("Database already populated")
+          }
+          ds.close()
+        })    
+      }
+    })
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
+
+
