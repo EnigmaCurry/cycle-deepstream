1abba574e7d036b1daff151dacd636c8de9a2377
Ryan McGuire <ryan@enigmacurry.com>
2017-04-02 17:03:56 -0400

    Create webpack build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+const __DEV__ = process.env.NODE_ENV === 'development'
+const __PROD__ = process.env.NODE_ENV === 'production'
+const __TEST__ = process.env.NODE_ENV === 'test'
+
+module.exports = {
+  entry: './index.ts',
+  output: {
+    filename: 'dist/index.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    historyApiFallback: true,
+    port: 3002
+  },
+  resolve: {
+    extensions: ['.ts','.js']
+  },
+  module: {
+    rules: [{ 
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      loader: 'ts-loader'
+    }]
+  },
+  devtool: __DEV__ ? 'eval-source-map' : 'source-map'
+}
